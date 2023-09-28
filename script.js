9d08dabf7bc1be78aeaca948352398510f792af9
kelleyhan25 <kelhan@bu.edu>
2023-09-28 15:07:57 -0400

    Added a clock on the right hand side of welcome page

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ flashcards.forEach((card) => {
         card.classList.toggle('flipped');
     });
 });
+
+
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+
+    var session = document.getElementById('session');
+
+    if(hrs >=12) {
+        session.innerHTML = 'PM';
+    } else {
+        session.innerHTML = 'AM';
+    }
+
+    if (hrs > 12) {
+        hrs = hrs - 12; 
+    }
+
+    var minText = (min < 10) ? '0' + min : min; 
+    var secText = (sec < 10) ? '0' + sec : sec; 
+    document.getElementById('hours').innerHTML = hrs; 
+    document.getElementById('minutes').innerHTML = minText; 
+    document.getElementById('seconds').innerHTML = secText; 
+}
+
+setInterval(displayTime, 10);
